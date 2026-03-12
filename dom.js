6110a275a759a170bc116077c3479accc5b8d806
ZAID <zaidkhan24082006@gmail.com>
2026-03-12 14:46:26 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,20 @@
+document.writeln("hello");
+
+function result(){
+//     let inputdata = document.getElementById("inputText").value;
+
+    let changecolor = document.getElementById('text');
+    changecolor.style.color="white";
+
+    let bodycolor = document.getElementsByTagName('body')[0];
+    bodycolor.style.backgroundColor = "red";
+
+    document.getElementById('text').style.fontSize = 500;
+}
+
+let para = document.getElementById("paratext");
+
+// function changetextcolor(){
+//     para.style.backgroundColor = "#" +Math.floor(Math.random() * 16777777777);
+// }
+// para.addEventListener('mouseover',changetextcolor);
