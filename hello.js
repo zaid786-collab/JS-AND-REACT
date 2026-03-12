2ece2109c14f3543644929e9d93580022de181f8
ZAID <zaidkhan24082006@gmail.com>
2026-03-12 13:22:16 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("hello");
+alert("Intro to class");
+
+// Variable in J.S : Var,let,Const.
+// var is globally scoped but let and const are block scope only.
+// value declared with const cannot be updated,it is essential to initilise it at the time of declaration.
+
+// var cab be reinitilised with same varaible name which is its drawback.
+
+// There are 7 types of Data Type in J.S.
+
+// Object : 
